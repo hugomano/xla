openxla/openxla

[xla:gpu] Create a pathway between compile-time and runtime to pass TMAMetadata struct through.
PiperOrigin-RevId: 707471894

xla/service/gpu/runtime/kernel_thunk.cc
@@ -28,18 +28,19 @@ limitations under the License.
 #include "absl/strings/str_format.h"
 #include "absl/synchronization/mutex.h"
 #include "absl/types/span.h"
+#include "llvm/ADT/STLExtras.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/service/buffer_assignment.h"
 #include "xla/service/gpu/kernel_arguments.h"
 #include "xla/service/gpu/kernels/custom_kernel.h"
 #include "xla/service/gpu/launch_dimensions.h"
 #include "xla/service/gpu/runtime/thunk.h"
+#include "xla/service/gpu/runtime/tma_metadata.h"
 #include "xla/service/gpu/stream_executor_util.h"
 #include "xla/stream_executor/device_memory.h"
 #include "xla/stream_executor/kernel.h"
 #include "xla/stream_executor/launch_dim.h"
 #include "xla/stream_executor/stream_executor.h"
-#include "tsl/platform/logging.h"
 #include "tsl/platform/statusor.h"
 
 namespace xla {
@@ -53,12 +54,13 @@ KernelThunk::KernelThunk(const HloInstruction* instr, std::string kernel_name,
                          absl::Span<const KernelArgument> kernel_arguments,
                          LaunchDimensions launch_dimensions,
                          std::optional<se::ClusterDim> cluster_dim,
-                         int64_t shmem_bytes)
+                         int64_t shmem_bytes, TmaMetadata tma_metadata)
     : Thunk(Kind::kKernel, Thunk::ThunkInfo::WithProfileAnnotation(instr)),
       kernel_name_(std::move(kernel_name)),
       launch_dimensions_(std::move(launch_dimensions)),
       cluster_dim_(std::move(cluster_dim)),
-      shmem_bytes_(shmem_bytes) {
+      shmem_bytes_(shmem_bytes),
+      tma_metadata_(std::move(tma_metadata)) {
   args_.reserve(kernel_arguments.size());
   written_.reserve(kernel_arguments.size());
   for (const auto& kernel_argument : kernel_arguments) {
@@ -137,11 +139,19 @@ absl::Status KernelThunk::ExecuteOnStream(const ExecuteParams& params) {
 
   VLOG(3) << "Launching " << kernel->name();
   absl::InlinedVector<se::DeviceMemoryBase, 4> buffer_args;
-  for (const BufferAllocation::Slice& arg : args_) {
+  for (const auto& [idx, arg] : llvm::enumerate(args_)) {
     se::DeviceMemoryBase buf = params.buffer_allocations->GetDeviceAddress(arg);
     VLOG(3) << "  Arg: alloc #" << arg.index() << ", offset: " << arg.offset()
             << ": " << buf.opaque() << " (" << buf.size() << "B)";
-    buffer_args.push_back(buf);
+
+    if (tma_metadata_.arg_index_to_tma_info.contains(idx)) {
+      TmaDescriptor tma_desc = tma_metadata_.arg_index_to_tma_info.at(idx);
+      TF_ASSIGN_OR_RETURN(auto tensor_map,
+                          executor->CreateTensorMap(tma_desc, buf.opaque()));
+      buffer_args.push_back(tensor_map);
+    } else {
+      buffer_args.push_back(buf);
+    }
   }
 
   if (VLOG_IS_ON(100)) {

hugomano/xla

xla/backends/gpu/runtime/kernel_thunk.cc
@@ -28,6 +28,7 @@ limitations under the License.
 #include "absl/strings/str_format.h"
 #include "absl/synchronization/mutex.h"
 #include "absl/types/span.h"
+#include "llvm/ADT/STLExtras.h"
 #include "xla/backends/gpu/runtime/thunk.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/service/buffer_assignment.h"
@@ -36,9 +37,11 @@ limitations under the License.
 #include "xla/service/gpu/launch_dimensions.h"
 #include "xla/service/gpu/stream_executor_util.h"
 #include "xla/stream_executor/device_memory.h"
+#include "xla/stream_executor/gpu/tma_metadata.h"
 #include "xla/stream_executor/kernel.h"
 #include "xla/stream_executor/launch_dim.h"
 #include "xla/stream_executor/stream_executor.h"
+#include "xla/tsl/platform/statusor.h"
 #include "tsl/platform/logging.h"
 #include "tsl/platform/statusor.h"
 
@@ -53,12 +56,14 @@ KernelThunk::KernelThunk(const HloInstruction* instr, std::string kernel_name,
                          absl::Span<const KernelArgument> kernel_arguments,
                          LaunchDimensions launch_dimensions,
                          std::optional<se::ClusterDim> cluster_dim,
-                         int64_t shmem_bytes)
+                         int64_t shmem_bytes,
+                         stream_executor::gpu::TmaMetadata tma_metadata)
     : Thunk(Kind::kKernel, Thunk::ThunkInfo::WithProfileAnnotation(instr)),
       kernel_name_(std::move(kernel_name)),
       launch_dimensions_(std::move(launch_dimensions)),
       cluster_dim_(std::move(cluster_dim)),
-      shmem_bytes_(shmem_bytes) {
+      shmem_bytes_(shmem_bytes),
+      tma_metadata_(std::move(tma_metadata)) {
   args_.reserve(kernel_arguments.size());
   written_.reserve(kernel_arguments.size());
   for (const auto& kernel_argument : kernel_arguments) {
@@ -137,11 +142,21 @@ absl::Status KernelThunk::ExecuteOnStream(const ExecuteParams& params) {
 
   VLOG(3) << "Launching " << kernel->name();
   absl::InlinedVector<se::DeviceMemoryBase, 4> buffer_args;
-  for (const BufferAllocation::Slice& arg : args_) {
+  for (const auto& [idx, arg] : llvm::enumerate(args_)) {
     se::DeviceMemoryBase buf = params.buffer_allocations->GetDeviceAddress(arg);
     VLOG(3) << "  Arg: alloc #" << arg.index() << ", offset: " << arg.offset()
             << ": " << buf.opaque() << " (" << buf.size() << "B)";
-    buffer_args.push_back(buf);
+    if (tma_metadata_.arg_index_to_tma_info.contains(idx)) {
+      auto tma_desc = tma_metadata_.arg_index_to_tma_info.at(idx);
+      TF_ASSIGN_OR_RETURN(auto tensor_map,
+                          executor->CreateTensorMap(tma_desc, buf.opaque()));
+      VLOG(3) << "  Using TensorMap for arg #" << arg.index() << ": "
+              << tma_desc.ToString() << "; buffer: " << tensor_map.opaque()
+              << " (" << tensor_map.size() << "B)";
+      buffer_args.push_back(tensor_map);
+    } else {
+      buffer_args.push_back(buf);
+    }
   }
 
   if (VLOG_IS_ON(100)) {